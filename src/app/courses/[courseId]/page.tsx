748704d95aa57eca847586f7d663698717a85459
luci379 <120455115+luci379@users.noreply.github.com>
2025-10-17 09:50:58 -0500

    Courses courseid (#16)
    * set up: Shadcn/ui, react hook form, zod -luci :)
    
    * courses scaffolding
    
    * courses[courseId]
    
    * course-courseid
    
    ---------
    
    Co-authored-by: lucia <lucianit3@gmail.com>
    Co-authored-by: Joshua Silva <72359611+joshuasilva414@users.noreply.github.com>

--- src/app/courses/[courseId]/page.tsx
+++ src/app/courses/[courseId]/page.tsx
@@ -1,7 +1,8 @@
-export default function CoursesPage() {
-  return (
-    <div>
-      This is the Courses/CoursesIDpage scaffold
-    </div>
-  )
+export default async function Page({
+    params,
+}: {
+    params: Promise<{ courseId: string }>;
+}) {
+    const { courseId } = await params;
+    return <div>Managing tag: {courseId}</div>;
 }
