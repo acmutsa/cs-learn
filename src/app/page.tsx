f9dff183e1b40ed007ffe061473b49c7eab280aa
James Nguyen <dangkhoan1108@gmail.com>
2025-11-05 20:38:05 -0600

    remove unnecessary thing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,22 +6,10 @@ export default function Home() {
   var isSignedIn = false;
   var mockUser = null;
 
-  /*if testing just commented out this block of code S
-  isSignedIn = true;
-  mockUser = {
-    name: "Frieren",
-    email: "Frieren@gmail.com",
-    avatar: "frieren.jpg",
-    profileHref: "/profile",
-    coursesHref: "/user/courses",
-    historyHref: "/courses/history",
-  }*/
-  ///////////////////
-
   return (
     <div className="min-h-screen bg-gradient-to-b from-slate-50 via-white to-white text-slate-900">
       <header className="border-b border-slate-200 bg-white/70 backdrop-blur">
-        <Navigation isSignedIn={isSignedIn} User={mockUser}/> {}
+        <Navigation isSignedIn={isSignedIn}/> {}
       </header>
     </div>
   );
